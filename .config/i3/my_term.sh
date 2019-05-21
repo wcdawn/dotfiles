3e5328433a7a989196149d4e05c932deb49b2b2c
William Dawn <wcdawn@ncsu.edu>
2019-05-21 15:49:35 -0600

    finally fixing first call to terminal after reboot

--- .config/i3/my_term.sh
+++ .config/i3/my_term.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+FILENAME=/tmp/whereami
+
 # spawn a terminal where i am right now
 # in $HOME/.bashrc -- export PROMPT_COMMAND="pwd > /tmp/whereami"
 # using this temporary string file to keep track of location
-WHEREAMI=$(cat /tmp/whereami)
-urxvt -cd $WHEREAMI 
+if [ -e $FILENAME ]
+then
+  WHEREAMI=$(cat $FILENAME)
+  urxvt -cd $WHEREAMI 
+else
+  urxvt
+fi
