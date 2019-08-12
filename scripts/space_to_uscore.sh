d48aaeb6b18a1f6a9ef0b9c50f2bbe16d1b3fc01
William Dawn <wcdawn@ncsu.edu>
2019-08-12 11:15:30 -0400

    script to convert spaces to underscores in filenames

---
+++ scripts/space_to_uscore.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# if no filename input, operate recursively on the directory
+# otherwise, operate only on the filename(s) specified
+
+OIFS="$IFS"
+IFS=$'\n'
+
+if [ $# -eq 0 ]
+then
+  # no argument
+  LIST="./*"
+else
+  LIST="$*"
+fi
+
+for FNAME in $LIST
+do
+  if [[ $FNAME == *' '* ]]
+  then
+    FNAME_NEW=$(echo $FNAME | sed 's/ /_/')
+    mv $FNAME $FNAME_NEW
+  fi
+done
+
+IFS="$OIFS"
