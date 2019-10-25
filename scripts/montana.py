ef15cd2d8b3f668b0aed24795ab3279264304d97
William Dawn <wcdawn@ncsu.edu>
2019-10-25 11:07:50 -0400

    working on formatting Missoula and Flathead

--- scripts/montana.py
+++ scripts/montana.py
@@ -1,8 +1,21 @@
 #!/bin/python
 
+# date and time
 import datetime
 import pytz
+
+# curl
+import requests
 
 now = datetime.datetime.now(pytz.timezone('America/Denver'))
 print('The time in Montana is:')
 print(now.strftime('%a %w %b %Y %H:%M:%S %Z'))
+
+r = requests.get('http://wttr.in/Missoula?0pqu')
+missoula = r.text.splitlines()
+
+r = requests.get('http://wttr.in/Flathead?0pqu')
+flathead = r.text.splitlines()
+
+for i in range(len(missoula)):
+    print('{:30s}    {:s}'.format(missoula[i], flathead[i]))
