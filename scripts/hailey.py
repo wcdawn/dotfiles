32d2f30f586bb3b0443ec2c868b8eb2c40723397
William Dawn <wcdawn@ncsu.edu>
2019-11-19 10:14:34 -0500

    allowing for command line arguments

--- scripts/hailey.py
+++ scripts/hailey.py
@@ -1,5 +1,8 @@
 #!/bin/python
 
+# argument processing
+import sys, getopt
+
 # date and time
 import datetime
 import pytz
@@ -15,11 +18,36 @@ from PIL import Image
 # webcams
 import webcam
 
+# default options
+doWeather = True
+doImage = True
+whichWebcam = 0
+
+# logical input
+# -w -- do weather
+# -i -- do image
+# integer input
+# -c -- (1,2) for (lake_view, tree_view) webcam
+
+# arguemnt processing
+myopts, args = getopt.getopt(sys.argv[1:], 'w:i:c:')
+# o -- option
+# a -- argument
+for o, a in myopts:
+    if o == '-w':
+        doWeather = bool(int(a))
+    elif o == '-i':
+        doImage = bool(int(a))
+    elif o == '-c':
+        whichWebcam = int(a)
+    else:
+        print(o, a)
+        print('Usage: {:s}')
+
 # date/time in Montana
 now = datetime.datetime.now(pytz.timezone('America/Denver'))
 print('The time in Montana is:')
 print(now.strftime('%a %w %b %Y %H:%M:%S %Z'))
-
 
 # days until
 fname = '/home/wcdawn/hailey/next_visit.txt'
@@ -36,32 +64,37 @@ print()
 print('Days until next visit: {:d}'.format(diff.days + 1))
 
 # display an image
-image_fname = '/home/wcdawn/hailey/christmas_pic/portland_canard.jpg'
-image = Image.open(image_fname)
-maxsize = (640, 640)
-image.thumbnail(maxsize, PIL.Image.ANTIALIAS)
-image.show()
+if (doImage):
+    image_fname = '/home/wcdawn/hailey/christmas_pic/portland_canard.jpg'
+    image = Image.open(image_fname)
+    maxsize = (640, 640)
+    image.thumbnail(maxsize, PIL.Image.ANTIALIAS)
+    image.show()
 
 # weather
-location_dict = {
-    'Missoula': [46.856339, -113.995292],
-    'Flathead': [47.876957, -114.032290]}
+if (doWeather):
+    location_dict = {
+        'Missoula': [46.856339, -113.995292],
+        'Flathead': [47.876957, -114.032290]}
 
-weather_list = []
+    weather_list = []
 
-for key in location_dict:
-    weather_list.append(weatherFormat(key, location_dict[key][0], 
-            location_dict[key][1]))
+    for key in location_dict:
+        weather_list.append(weatherFormat(key, location_dict[key][0], 
+                location_dict[key][1]))
 
-padded_width = 40
-for i in range(len(weather_list[0])):
-    blank_size = padded_width - len(ansi_escape.sub('', weather_list[0][i]))
-    print(weather_list[0][i] + blank_size * ' ' + weather_list[1][i])
-
+    padded_width = 40
+    for i in range(len(weather_list[0])):
+        blank_size = padded_width - len(ansi_escape.sub('', weather_list[0][i]))
+        print(weather_list[0][i] + blank_size * ' ' + weather_list[1][i])
 
 # webcams
+# http://webcam.flbs.umt.edu/view/viewer_index.shtml?id=2731
 lake_view = 'http://webcam.flbs.umt.edu/mjpg/video.mjpg'
+# http://webcam2.flbs.umt.edu/view/viewer_index.shtml?id=4824
 tree_view = 'http://webcam2.flbs.umt.edu/mjpg/video.mjpg'
 
-#webcam.dispWebcam(lake_view)
-#webcam.dispWebcam(tree_view)
+if (whichWebcam == 1):
+    webcam.dispWebcam(lake_view)
+elif (whichWebcam == 2):
+    webcam.dispWebcam(tree_view)
