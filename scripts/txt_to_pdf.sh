3944ff58a6bf144705a60db5ca12b921d07632c5
William Dawn <wcdawn@ncsu.edu>
2020-05-02 18:42:32 -0600

    adding options for quick running with font and orientation

--- scripts/txt_to_pdf.sh
+++ scripts/txt_to_pdf.sh
@@ -14,6 +14,12 @@ usage()
   exit 0
 }
 
+build_font()
+{
+  local FONT_SIZE=$1
+  echo 'Courier'${FONT_SIZE}
+}
+
 pdf_trunc()
 {
   local ORIENTATION=$1
@@ -24,7 +30,6 @@ pdf_trunc()
     ps2pdf - > $NAME.pdf) 2>&1)
   local TRUNC=$(echo $OUT | grep 'truncated')
   echo $TRUNC
-
 }
 
 # check for dependencies
@@ -40,6 +45,45 @@ then
   exit 1
 fi
 
+DO_QUICK=''
+ORIENTATION='-R' # default to portrait
+FONT_SIZE=10 # default to enscript default
+while getopts "s:o:h" opt # trailing colon specifies an argument is required
+do
+  case ${opt} in
+    s) # font size
+      FONT_SIZE="$OPTARG"
+      DO_QUICK=${DO_QUICK}'s'
+      ;;
+    o) # orientation
+      if [ "$OPTARG" == 'p' ]
+      then
+        ORIENTATION='-R'
+      elif [ "$OPTARG" == 'l' ]
+      then
+        ORIENTATION='-r'
+      else
+        echo 'invalid orientation option'
+        echo 'p for portrait'
+        echo 'l for landscape'
+        exit 1
+      fi
+      DO_QUICK=${DO_QUICK}'o'
+      ;;
+    h) # help
+      usage
+      exit 0
+      ;;
+    \?) # error
+      echo 'invalid argument'
+      usage
+      exit 1
+  esac
+done
+
+shift $((OPTIND - 1)) # shift to ignore already parsed command line arguments
+
+# parse file name
 FNAME=$1
 if [ -z $FNAME ]
 then
@@ -47,10 +91,15 @@ then
 fi
 NAME=$(echo $FNAME | sed 's/\.txt//')
 
-ORIENTATION='-R' # default to portrait
+if [ "$DO_QUICK" ]
+then
+  FONT=$(build_font $FONT_SIZE)
+  TRUNC=$(pdf_trunc $ORIENTATION $FONT $NAME)
+  echo $TRUNC
+  exit 0
+fi
 
-FONT_SIZE=10
-FONT='Courier'${FONT_SIZE}
+FONT=$(build_font $FONT_SIZE)
 
 OUT=$((enscript -c ${ORIENTATION} -f${FONT} $NAME.txt --output=- | ps2pdf - > $NAME.pdf) 2>&1)
 TRUNC=$(echo $OUT | grep 'truncated')
@@ -66,7 +115,7 @@ TRUNC=$(pdf_trunc $ORIENTATION $FONT $NAME)
 while [ "$TRUNC" > /dev/null ]
 do
   FONT_SIZE=$((FONT_SIZE - 1))
-  FONT='Courier'${FONT_SIZE}
+  FONT=$(build_font $FONT_SIZE)
 
   TRUNC=$(pdf_trunc $ORIENTATION $FONT $NAME)
 
