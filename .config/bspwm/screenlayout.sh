247149ebd523b0c9101dcca24e513c16849ae9b4
William Dawn <wcdawn@ncsu.edu>
2020-05-04 14:12:37 -0600

    adding some basic utilities

--- .config/bspwm/screenlayout.sh
+++ .config/bspwm/screenlayout.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-
-xrandr --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal \
-  --output HDMI3 --mode 1920x1080 --pos 1920x0 --rotate normal
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-4 --off --output DP-5 --off
