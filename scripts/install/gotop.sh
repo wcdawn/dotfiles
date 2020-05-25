e5801e309d8a98abf24a72bcddf8bc0cf45e0435
William Dawn <wcdawn@ncsu.edu>
2020-05-25 09:50:36 -0600

    updating to match the script as committed in the gotop repo

--- scripts/install/gotop.sh
+++ scripts/install/gotop.sh
@@ -1,31 +1,44 @@
 #!/bin/sh
+#
+# Builds the gotop executable on machines where Go isn't installed,
+# or is the wrong version, and you don't have root access to upgrade or
+# install Go.
+#
+# You can run this without cloning the entire gotop repository (the script
+# will do this for you.)
 
-VERSION='1.14.2'
+set -x
+
+VERSION='1.14.2'     # Go version needed to build
 OS='linux'
 ARCH='amd64'
+BUILDDIR=/tmp/gotop-build
+INSTALLDIR=${HOME}/bin
 
 GO_NAME=go${VERSION}.${OS}-${ARCH}
 
-mkdir -p $HOME/pkg
-cd $HOME/pkg
+mkdir -p $BUILDDIR
+cd $BUILDDIR
 
 curl https://dl.google.com/go/${GO_NAME}.tar.gz --output ./${GO_NAME}.tar.gz
 
 tar -vxzf ${GO_NAME}.tar.gz
 rm ${GO_NAME}.tar.gz
 
-PATH=$HOME/pkg/go/bin:$PATH
+PATH=$BUILDDIR/go/bin:$PATH
 
-go env -w GOPATH=$HOME/pkg # otherwise go would create a directory in $HOME
+go env -w GOPATH=$BUILDDIR # otherwise go would create a directory in $HOME
 
 rm -rf ./gotop
 git clone https://github.com/xxxserxxx/gotop.git
 cd ./gotop
 go build -o gotop ./cmd/gotop
 
-go clean -modcache # otherwise $HOME/pkg/pkg would need sudo permissions to remove
+go clean -modcache # otherwise $BUILDDIR/pkg would need sudo permissions to remove
 
-ln -sf $HOME/pkg/gotop/gotop $HOME/bin/gotop
+mkdir -p $INSTALLDIR
+mv gotop ${INSTALLDIR}/gotop
 
-rm -rf $HOME/pkg/go
-rm -rf $HOME/pkg/pkg # installed as part of go
+rm -rf $BUILDDIR
+
+printf "gotop installed in ${INSTALLDIR}/gotop\n"
